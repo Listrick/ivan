c45181c31bcf607403f1063947985718ed228b74
Listrick <listrick7@gmial.com>
2021-07-09 22:31:32 -0500

    portafolio

---
+++ assets/js/main.js
@@ -0,0 +1,95 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+/* Button effect */
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(btn => {
+    btn.addEventListener('click', function(e){
+
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove()
+        }, 1000);
+    })
+})
+
+
+/*SCROLL HOME*/
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200});
+
+
+/* ====PORTFOLIO==== */
+const grid = new Muuri('.grid', {
+    layout: {
+        rounding: false
+    }
+});
+
+/* FILTRADO POR CATEGORIA */
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+
+    const enlaces = document.querySelectorAll('#categorias a');
+    enlaces.forEach( (elemento) => {
+        elemento.addEventListener('click', (evento) => {
+            evento.preventDefault();
+            enlaces.forEach((enlace) => enlace.classList.remove('activo'));
+            evento.target.classList.add('activo');
+
+			const categoria = evento.target.innerHTML.toLowerCase();
+			categoria === 'todos' ? grid.filter('[data-categoria]') : grid.filter(`[data-categoria="${categoria}"]`);
+
+        });
+    });
+
+    /* BARRA DE BUSQUEDA */
+    document.querySelector('#barra__busqueda').addEventListener('input', (evento) => {
+        const busqueda = evento.target.value;
+        grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
+    });
+    
+});
+
